f50b9a2555d4af49e41f1b4c0576065c528ac930
Philippe Gras <philippe.gras@cern.ch>
2015-09-27 15:45:40 +0200

    Bug fix
    JEC reapplicaion modules needed to appear in the processing path before Tupel module

--- ntuple_production/grow_baobabs_cfg.py
+++ ntuple_production/grow_baobabs_cfg.py
@@ -1,18 +1,61 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("GrowBoababs")
-process.source = cms.Source("PoolSource")
+
+process.source = cms.Source("PoolSource",
+#                            fileNames =  cms.untracked.vstring(
+#'/store/data/Run2015C/SinglePhoton/MINIAOD/PromptReco-v1/000/253/620/00000/6C6DC194-1C40-E511-A671-02163E014324.root')
+)
+#process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
 
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string('ntuple.root' )
 )
+
+reapply_jec=False
+isMC=False
+mcGlobalTag='74X_mcRun2_asymptotic_v2'
+dataGlobalTag='74X_dataRun2_v2'
+
+#--------------------------------------
+#JEC
+#
+if reapply_jec:
+  process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+  from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+  if isMC:
+    process.GlobalTag = GlobalTag(process.GlobalTag, mcGlobalTag, '')
+  else:
+    process.GlobalTag = GlobalTag(process.GlobalTag, dataGlobalTag, '')
+  #endif iMC
+
+  from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import patJetCorrFactorsUpdated
+  process.patJetCorrFactorsReapplyJEC = patJetCorrFactorsUpdated.clone(
+    src = cms.InputTag("slimmedJets"),
+    levels = ['L1FastJet', 
+              'L2Relative',
+              'L3Absolute'],
+    payload = 'AK4PFchs' ) # Make sure to choose the appropriate levels and payload here!
+
+  from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import patJetsUpdated
+  process.patJetsReapplyJEC = patJetsUpdated.clone(
+    jetSource = cms.InputTag("slimmedJets"),
+    jetCorrFactorsSource = cms.VInputTag(cms.InputTag("patJetCorrFactorsReapplyJEC"))
+    )
+
+  jetSrc = "patJetsReapplyJEC"
+else:
+  jetSrc = "slimmedJets"
+#endif reapply_jec
+#
+#--------------------------------------------
 
 process.tupel = cms.EDAnalyzer("Tupel",
   triggerEvent = cms.InputTag( "patTriggerEvent" ),
   photonSrc   = cms.untracked.InputTag("slimmedPhotons"),
   electronSrc = cms.untracked.InputTag("slimmedElectrons"),
   muonSrc     = cms.untracked.InputTag("slimmedMuons"),
-  jetSrc      = cms.untracked.InputTag("slimmedJets"),
+  jetSrc      = cms.untracked.InputTag(jetSrc),
   metSrc      = cms.untracked.InputTag("patMETsPF"),
   genSrc      = cms.untracked.InputTag("prunedGenParticles"),
   gjetSrc       = cms.untracked.InputTag('slimmedGenJets'),
@@ -32,9 +75,12 @@ process.goodOfflinePrimaryVertices = cms.EDFilter(
     src=cms.InputTag('offlineSlimmedPrimaryVertices')
     )
 
-process.p = cms.Path(
- process.tupel 
-)
+process.p = cms.Path()
+
+if reapply_jec:
+  process.p += cms.Sequence( process.patJetCorrFactorsReapplyJEC + process.patJetsReapplyJEC )
+
+process.p += process.tupel
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 10
@@ -42,6 +88,10 @@ process.MessageLogger.suppressWarning = cms.untracked.vstring('ecalLaserCorrFilt
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.options.allowUnscheduled = cms.untracked.bool(True)
 
+#process.out = cms.OutputModule("PoolOutputModule",
+#                               fileName = cms.untracked.string('edm_out.root'))
+#
+#process.outpath = cms.EndPath(process.out)
 
 #iFileName = "fileNameDump_cfg.py"
 #file = open(iFileName,'w')

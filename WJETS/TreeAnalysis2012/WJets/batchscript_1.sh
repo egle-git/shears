6820fbe11c74280d2fc602238bba039de188b546
ocalan <kadir.ocalan@cern.ch>
2015-10-23 11:38:11 +0200

    Removing WJETS

--- WJETS/TreeAnalysis2012/WJets/batchscript_1.sh
+++
@@ -1,8 +0,0 @@
-
-
-#!/bin/sh
-cd /afs/cern.ch/work/b/bhawan/private/Tom/Tom_new/CMSSW_5_3_0/src/TreeAnalysis2012/WJets
-eval `scramv1 runtime -sh`
-root -b -q runDYJets.cc++
-rfcp *.root /afs/cern.ch/work/b/bhawan/private/Tom/Tom_new/CMSSW_5_3_0/src/TreeAnalysis2012/WJets/HistoFiles/
-

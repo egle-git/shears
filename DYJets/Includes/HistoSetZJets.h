e4b69c0bfbd8c4f9776e9cb99efde7898ab9585d
AnastasiaGrebenyuk <grebea@mail.desy.de>
2015-12-07 16:26:57 +0100

    unfolding unc

--- DYJets/Includes/HistoSetZJets.h
+++ DYJets/Includes/HistoSetZJets.h
@@ -35,6 +35,7 @@ class HistoSetZJets {
         TH1D *NumberPFcandidates;
         TH1D *ZMass_lowDeltaR;
         TH1D *ZMass_Zinc0jet;
+        TH1D *ZMass_Zinc1jet;
         TH1D *ZMassFrom60_Zinc0jet;
         TH1D *genZMass_Zinc0jet;
         TH1D *ZPt_Zinc0jet;
@@ -74,6 +75,7 @@ class HistoSetZJets {
         TH1D *ZEta_Zexc2jet;
 
         TH1D *lepEta_Zinc0jet;
+        TH1D *lepEta_Zinc1jet;
         TH1D *lepEtaUpTo4_Zinc0jet;
         TH1D *genlepEta_Zinc0jet;
 
@@ -169,13 +171,16 @@ class HistoSetZJets {
         TH1D *FirstJetPhi_Zexc1jet;
         TH1D *SecondJetPhi_Zexc2jet;
         TH1D *lepPt_Zinc0jet;
+        TH1D *lepPt_Zinc1jet;
         TH1D *lepPtFrom15_Zinc0jet;
         TH1D *genlepPt_Zinc0jet;
         TH1D *lepPt_Zexc0jet;
         TH1D *dPhiLeptons_Zexc0jet;
         TH1D *dPhiLeptons_Zinc0jet;
+        TH1D *dPhiLeptons_Zinc1jet;
         TH1D *dEtaLeptons_Zexc0jet;
         TH1D *dEtaLeptons_Zinc0jet;
+        TH1D *dEtaLeptons_Zinc1jet;
         TH1D *dRLeptons_Zinc0jet;
         TH1D *SpTLeptons_Zexc0jet;
         TH1D *SpTLeptons_Zexc1jet;

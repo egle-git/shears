0e0d6bbeadd05c98038034fc3326c9d0610f4805
Nicolas <npostiau@ulb.ac.be>
2017-06-28 16:20:24 +0200

    Object selection part moved to a separate namespace

--- HZZ2l2nu/Makefile
+++ HZZ2l2nu/Makefile
@@ -1,5 +1,5 @@
-runHZZanalysis: runHZZ2l2nu.cc HZZ2l2nuLooper.o SmartSelectionMonitor.o Utils.o
-	g++ runHZZ2l2nu.cc -L/cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-ikhhed6/lib -lGui -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lMultiProc -pthread -lm -ldl -rdynamic -pthread -std=c++11 -Wno-deprecated-declarations -m64 -I/cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-ikhhed6/include HZZ2l2nuLooper.o SmartSelectionMonitor.o Utils.o -o runHZZanalysis
+runHZZanalysis: runHZZ2l2nu.cc HZZ2l2nuLooper.o SmartSelectionMonitor.o Utils.o ObjectSelection.o
+	g++ runHZZ2l2nu.cc -L/cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-ikhhed6/lib -lGui -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lMultiProc -pthread -lm -ldl -rdynamic -pthread -std=c++11 -Wno-deprecated-declarations -m64 -I/cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-ikhhed6/include HZZ2l2nuLooper.o SmartSelectionMonitor.o Utils.o ObjectSelection.o -o runHZZanalysis
 
 HZZ2l2nuLooper.o: Sources/HZZ2l2nuLooper.cc Includes/HZZ2l2nuLooper.h
 	g++ -c Sources/HZZ2l2nuLooper.cc -L/cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-ikhhed6/lib -lGui -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lMultiProc -pthread -lm -ldl -rdynamic -pthread -std=c++11 -Wno-deprecated-declarations -m64 -I/cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-ikhhed6/include -o HZZ2l2nuLooper.o
@@ -9,6 +9,9 @@ SmartSelectionMonitor.o: Sources/SmartSelectionMonitor.cc Includes/SmartSelectio
 
 Utils.o: Sources/Utils.cc Includes/Utils.h
 	g++ -c Sources/Utils.cc -L/cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-ikhhed6/lib -lGui -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lMultiProc -pthread -lm -ldl -rdynamic -pthread -std=c++11 -Wno-deprecated-declarations -m64 -I/cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-ikhhed6/include -o Utils.o
+
+ObjectSelection.o: Sources/ObjectSelection.cc Includes/ObjectSelection.h
+	g++ -c Sources/ObjectSelection.cc -L/cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-ikhhed6/lib -lGui -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lMultiProc -pthread -lm -ldl -rdynamic -pthread -std=c++11 -Wno-deprecated-declarations -m64 -I/cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-ikhhed6/include -o ObjectSelection.o
 
 clean:
 	rm -f *.o *.d
--- HZZ2l2nu/Sources/HZZ2l2nuLooper.cc
+++ HZZ2l2nu/Sources/HZZ2l2nuLooper.cc
@@ -2,6 +2,7 @@
 #include "../Includes/HZZ2l2nuLooper.h"
 #include "../Includes/SmartSelectionMonitor.h"
 #include "../Includes/Utils.h"
+#include "../Includes/ObjectSelection.h"
 #include <TH1.h>
 #include <TH2.h>
 #include <TFile.h>
@@ -69,85 +70,19 @@ void HZZ2l2nuLooper::Loop()
       mon.fillHisto("nb_e","all",ElPt->size(),weight);
       mon.fillHisto("pile-up","all",EvtPuCnt,weight);
 
-      //Leptons passing final cuts, or additional leptons veto
-      vector<TLorentzVector> selElectrons;
-      vector<TLorentzVector> selMuons;
-      vector<TLorentzVector> extraElectrons;
-      vector<TLorentzVector> extraMuons;
-      vector<TLorentzVector> selPhotons;
-      vector<TLorentzVector> tagJets;
-      vector<TLorentzVector> selJets;
-      vector<double> btags;
+      vector<TLorentzVector> selElectrons; //Leptons passing final cuts
+      vector<TLorentzVector> selMuons; //Muons passing final cuts
+      vector<TLorentzVector> extraElectrons; //Additional electrons, used for veto
+      vector<TLorentzVector> extraMuons; //Additional muons, used for veto
+      vector<TLorentzVector> selPhotons; //Photons
+      vector<TLorentzVector> tagJets; //Jets passing Id and cleaning, with |eta|<4.7 and pT>15GeV. Used for the DeltaPhi cut.
+      vector<TLorentzVector> selJets; //Jets passing Id and cleaning, with |eta|<4.7 and pT>30GeV. Used for jet categorization.
+      vector<double> btags; //B-Tag discriminant, recorded for selJets with |eta|<2.5. Used for b-tag veto.
 
-      for(int i = 0 ; i<ElPt->size() ; i++){ //Electrons
-         bool passEta = false, passIso = false, passId = false, passPt = false, passLoosePt = false, passLooseId = false;
-	 TLorentzVector currentLepton; currentLepton.SetPtEtaPhiE(ElPt->at(i),ElEta->at(i),ElPhi->at(i),ElE->at(i));
-	 //Id //Very temporary!!! Used without much cross-checking.
-	 passId = ElId->at(i) & (1<<17);
-	 passLooseId = ElId->at(i) & (1<<16);
-	 int eta = fabs(ElEtaSc->at(i));//I took the supercluster eta since it's really the geometry which is taken here.
-	 passEta = (eta<=2.5 && (eta>=1.5660 || eta<=1.4442));
-	 //Iso //We use ElPfIsoRho for now, we'll see after if it's mandatory to refine it. Iso is applied only for the "tight" selection, not for the extra lepton veto.
-	 if(eta>=1.5660 && ElPfIsoRho->at(i)<0.0646) passIso = true;
-	 if(eta<=1.4442 && ElPfIsoRho->at(i)<0.0354) passIso = true; //Numbers are taken from llvv_fwk and have not been checked.
-	 passPt = (currentLepton.Pt() >=25);
-	 passLoosePt = (currentLepton.Pt() >=10);
-	 if(passEta && passLooseId && passLoosePt && selElectrons.size()==2) extraElectrons.push_back(currentLepton); //No iso criteria for extra leptons.
-         if(passEta && passIso && passId && passPt && selElectrons.size()<2) selElectrons.push_back(currentLepton);
-      }
-      for(int i = 0 ; i<MuPt->size() ; i++){ //Muons
-         bool passEta = false, passIso = false, passId = false, passPt = false, passLoosePt = false, passLooseId = false, passSoftId = false, passSoftPt = false;
-	 TLorentzVector currentLepton; currentLepton.SetPtEtaPhiE(MuPt->at(i),MuEta->at(i),MuPhi->at(i),MuE->at(i));
-	 //Id //Very temporary!!! Used without much cross-checking.
-	 passId = MuIdTight->at(i) & (1<<0); //Look at the first vertex, hence the bit 0.
-	 passLooseId = MuId->at(i) & (1<<0);
-	 passSoftId = false; //Don't know yet how to implement this thing. FIXME
-	 int eta = fabs(MuEta->at(i));
-	 passEta = (eta<=2.4);
-	 //Iso //We use MuPfIso for now, we'll see after if it's mandatory to refine it. Iso is applied only for the "tight" selection, not for the extra lepton veto.
-	 passIso = (MuPfIso->at(i)<0.15); //Numbers are taken from llvv_fwk and have not been checked.
-	 passPt = (currentLepton.Pt() >=25);
-	 passLoosePt = (currentLepton.Pt() >=10);
-	 passSoftPt = (currentLepton.Pt() >=3);
-	 if(passEta && passLooseId && passLoosePt && selMuons.size()==2) extraMuons.push_back(currentLepton); //No iso criteria for extra leptons.
-	 if(passEta && !(passLooseId && passLoosePt) && passSoftId && passSoftPt && selMuons.size()==2) extraMuons.push_back(currentLepton); //Soft leptons. Need a particular cut?
-         if(passEta && passIso && passId && passPt && selMuons.size()<2) selMuons.push_back(currentLepton);
-      }
-      for(int i = 0 ; i<PhotPt->size() ; i++){ //Photons
-         TLorentzVector currentPhoton; currentPhoton.SetPtEtaPhiE(PhotPt->at(i),PhotEta->at(i),PhotPhi->at(i),PhotE3x3->at(i)); //photon energy???
-	 //FIXME Criteria need come here...
-	 selPhotons.push_back(currentPhoton);
-      }
-      for(int i =0 ; i<JetAk04Pt->size() ; i++){ //Jets
-         bool passPt = false, passSelPt = false, passEta = false, passTightEta = false, passId = false, passLeptonCleaning = false, passPhotonCleaning = false;
-	 TLorentzVector currentJet; currentJet.SetPtEtaPhiE(JetAk04Pt->at(i),JetAk04Eta->at(i),JetAk04Phi->at(i),JetAk04E->at(i));
-	 passPt = (currentJet.Pt() >=15);
-	 passSelPt = (currentJet.Pt() >=30);
-	 double eta = fabs(currentJet.Eta());
-	 passEta = (eta <= 4.7);
-	 passTightEta = (eta <= 2.5);
-	 if(eta<2.7){
-	    passId = (JetAk04Id->at(i) >= 1); //This case is simple, it corresponds exactly to what we apply for now
-	 }
-	 float nef = JetAk04NeutralEmFrac->at(i);
-	 float nhf = JetAk04NeutralHadAndHfFrac->at(i);
-	 float nnp = 20; //FIXME this variable could not be found in the tree, it has obviously to be fixed if we are to reproduce what stands in the AN.
-	 if(eta<3.0 && eta >=2.7){
-	    //passId = (nef > 0.01 && nhf < 0.98 && nnp > 2);
-	    passId = (JetAk04Id->at(i) >= 2); //Simpler criterium, but not equivalent to what is mentionned in the AN
-	 }
-	 if(eta>=3.0){
-	    //passId = (nef<0.90 && nnp > 10);
-	    passId = (JetAk04Id->at(i) >= 3); //Simpler criterium, but not equivalent to what is mentionned in the AN
-	 }
-	 double minDRlj(9999.); for(int ilep=0; ilep<selMuons.size(); ilep++) minDRlj = TMath::Min( minDRlj, utils::deltaR(currentJet,selMuons[ilep]) );
-	 for(int ilep=0; ilep<selElectrons.size(); ilep++) minDRlj = TMath::Min( minDRlj, utils::deltaR(currentJet,selElectrons[ilep]) );
-	 passLeptonCleaning = (minDRlj>=0.4);
-	 passPhotonCleaning = true; //FIXME Needs implementation of the photon cleaning
-	 if(passPt && passEta && passId && passLeptonCleaning && passPhotonCleaning) tagJets.push_back(currentJet);
-	 if(passSelPt && passEta && passId && passLeptonCleaning && passPhotonCleaning) selJets.push_back(currentJet);
-	 if(passSelPt && passTightEta && passId && passLeptonCleaning && passPhotonCleaning) btags.push_back(JetAk04BDiscCisvV2->at(i));
-      }
+      objectSelection::selectElectrons(selElectrons, extraElectrons, ElPt, ElEta, ElPhi, ElE, ElId, ElEtaSc, ElPfIsoRho);
+      objectSelection::selectMuons(selMuons, extraMuons, MuPt, MuEta, MuPhi, MuE, MuId, MuIdTight, MuPfIso);
+      objectSelection::selectPhotons(selPhotons, PhotPt, PhotEta, PhotPhi, PhotScRawE, PhotId);
+      objectSelection::selectJets(tagJets, selJets, btags, JetAk04Pt, JetAk04Eta, JetAk04Phi, JetAk04E, JetAk04Id, JetAk04NeutralEmFrac, JetAk04NeutralHadAndHfFrac, JetAk04BDiscCisvV2, selMuons, selElectrons, selPhotons);
 
       //Discriminate ee and mumu
       bool isEE = (selElectrons.size()==2); //2 good electrons

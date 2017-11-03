25f7e321faf47ecd586823f92b255c3f199e4892
Hugues <hbrun@cern.ch>
2017-11-03 14:56:25 +0100

    add the soft ID

--- Baobabs/src/Tupel.cc
+++ Baobabs/src/Tupel.cc
@@ -496,6 +496,7 @@ private:
   std::auto_ptr<std::vector<unsigned> > MuId_;
   std::map<std::string, unsigned>       MuIdMap_; //bit assignment
   std::auto_ptr<std::vector<unsigned> > MuIdTight_;
+  std::auto_ptr<std::vector<unsigned> > MuIdSoft_;
   std::map<std::string, unsigned>    	MuIdTightMap_; //bit assignment
   std::auto_ptr<std::vector<float> > 	MuCh_;
   std::auto_ptr<std::vector<float> > 	MuVtxZ_;
@@ -1609,14 +1610,21 @@ void Tupel::processMuons(){
       if(vtxx){
 	unsigned bit = 0;
 	unsigned muonTightIds = 0;
+    unsigned muonSoftIds = 0;
 	for (std::vector<reco::Vertex>::const_iterator vtx = pvHandle->begin(); vtx != pvHandle->end(); ++vtx){
 	  if(vtx->isValid() && !vtx->isFake() && mu[j].isTightMuon(*vtx)){
 	    muonTightIds |= (1 <<bit);
 	  }
+      if(mu[j].isSoftMuon(*vtx)){
+          muonSoftIds |= (1 <<bit);
+      }
+
 	  ++bit;
 	  if(bit > 31) break;
+    
 	}
 	MuIdTight_->push_back(muonTightIds);
+    MuIdSoft_->push_back(muonSoftIds);
       }
 
 
@@ -2593,6 +2601,7 @@ Tupel::beginJob()
   ADD_BRANCH(MuE);
   ADD_BRANCH(MuId);
   ADD_BRANCH_D(MuIdTight, "Muon tight id. Bit field, one bit per primary vertex hypothesis. Bit position corresponds to index in EvtVtx");
+  ADD_BRANCH_D(MuIdSoft, "Muon soft id. Bit field, one bit per primary vertex hypothesis. Bit position corresponds to index in EvtVtx");
   ADD_BRANCH(MuCh);
   ADD_BRANCH(MuVtxZ);
   ADD_BRANCH(MuDxy);

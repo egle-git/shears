9931c5a257fecc6fdbffac0c41171e409314f3ac
Hugues Brun <hbrun@cern.ch>
2017-11-10 12:13:10 +0100

    remove the duplicate line

--- HZZ2l2nu/Sources/SmartSelectionMonitor_hzz.cc
+++ HZZ2l2nu/Sources/SmartSelectionMonitor_hzz.cc
@@ -2,7 +2,6 @@
 
 bool SmartSelectionMonitor_hzz::declareHistos(){ //FIXME: Later, will take an array as input for the binnings.
    addHistogram(new TH1F("pile-up",";Number of PU events;Events",50,0,50));
-   addHistogram(new TH1F("pile-up",";Number of PU events;Events",50,0,50));
    addHistogram(new TH1F("truth-pile-up",";Truth number of PU events;Events",50,0,50));
    addHistogram(new TH1F("reco-vtx",";Number of reco vtx;Events",50,0,50));
    TH1F *h =(TH1F*) addHistogram(new TH1F("eventflow",";;Events",10,0,10));

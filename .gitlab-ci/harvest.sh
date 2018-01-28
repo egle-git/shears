067a817a6ce9f3aa91a749ce385253ac75520310
Hugues Brun <hbrun@cern.ch>
2018-01-28 11:47:33 +0100

    small fix for safety reasons

--- .gitlab-ci/harvest.sh
+++ .gitlab-ci/harvest.sh
@@ -10,6 +10,6 @@ export SCRAM_ARCH=slc6_amd64_gcc53
 cd HZZ2l2nu
 
 nbOfDiff=`ls result_* | grep png | wc -l`
-wget "https://slack.com/api/chat.postMessage?token=$1&channel=slackci&text=nb%20of%20differences%20found=$nbOfDiff&pretty=1&username=shearsDocker"
+wget -nv "https://slack.com/api/chat.postMessage?token=$1&channel=slackci&text=nb%20of%20differences%20found=$nbOfDiff&pretty=1&username=shearsDocker" >& scrnatch
 
 tar cfvz allComparisons.tar.gz result_* *Histos.root

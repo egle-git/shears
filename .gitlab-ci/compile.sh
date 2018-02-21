2fb0d6713a635e61d64a549137fe10c46e5d7f42
Louis Moureaux <lmoureau@ulb.ac.be>
2018-02-21 17:25:34 +0100

    Cleanup scripts in .gitlab-ci

--- .gitlab-ci/compile.sh
+++ .gitlab-ci/compile.sh
@@ -1,10 +1,6 @@
 #! /usr/bin/env bash
 
-shopt -s expand_aliases
-
-. /cvmfs/cms.cern.ch/cmsset_default.sh
-tar xfvz shearsCompilated.tar.gz
-cd CMSSW_8_0_26p1_testFullInstall && cmsenv && scramv1 b ProjectRename && scramv1 b -j 4 && cd -
+eval $(.gitlab-ci/cmsenv.sh)
 
 cd $1
 make

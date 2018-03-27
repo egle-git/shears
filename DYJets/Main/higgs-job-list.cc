10f9ca7a2e82d402a254c6ccec386a016558742a
Louis Moureaux <lmoureau@ulb.ac.be>
2018-03-27 02:17:26 +0200

    Add options to disable whole job categories

--- DYJets/Main/higgs-job-list.cc
+++ DYJets/Main/higgs-job-list.cc
@@ -1,14 +1,29 @@
+#include "options.h"
 #include "sample.h"
+
+/// \brief Retrieves the list of command-line options supported by this program.
+po::options_description options();
 
 int main(int argc, char **argv)
 {
     util::options opt;
     try {
-        opt.default_init(argc, argv, "higgs.yml", {/* No additional options */});
+        opt.default_init(argc, argv, "higgs.yml", {options()});
 
         const std::vector<data::sample> samples = data::sample::load(opt);
 
         for (const data::sample &sample : samples) {
+            if (opt.map.count("disable-data") > 0 && sample.type() == data::sample::data) {
+                continue;
+            }
+            if (opt.map.count("disable-mc") > 0 && sample.type() == data::sample::mc) {
+                continue;
+            }
+            if (opt.map.count("disable-background") > 0 &&
+                sample.type() == data::sample::background) {
+                continue;
+            }
+
             for (unsigned job = 0; job < sample.jobs(); ++job) {
                 std::cout << "Main/higgs-loop -s " << sample.name();
                 if (sample.jobs() > 1) {
@@ -24,4 +39,13 @@ int main(int argc, char **argv)
     }
 
     return EXIT_SUCCESS;
+}
+
+po::options_description options()
+{
+    po::options_description options("Sample selection options");
+    options.add_options()("disable-data", "Don't include jobs for data");
+    options.add_options()("disable-mc", "Don't include jobs for MC");
+    options.add_options()("disable-background", "Don't include jobs for background");
+    return options;
 }

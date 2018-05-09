602a0a28e25602437a7a3105db17dee3fb62d07c
Hugo Delannoy <hugo.ruben.delannoy@cern.ch>
2018-05-09 11:04:43 +0000

    Update Tupel.cc

--- Baobabs/src/Tupel.cc
+++ Baobabs/src/Tupel.cc
@@ -2437,8 +2437,8 @@ void Tupel::processPhotons(const edm::Event& iEvent, const edm::EventSetup& iSet
     PhotE2x5_->push_back(photon.e2x5());
     PhotE5x5_->push_back(photon.e5x5());
     PhotSigmaIetaIeta_->push_back(photon.sigmaIetaIeta());
-    PhotSigmaIetaIphi_->push_back(photon.sigmaIetaIphi());
-    PhotSigmaIphiIphi_->push_back(photon.sigmaIphiIphi());
+    PhotSigmaIetaIphi_->push_back(photon.showerShapeVariables().sigmaIetaIphi);
+    PhotSigmaIphiIphi_->push_back(photon.showerShapeVariables().sigmaIphiIphi);
 
     PhotEtaWidth_->push_back(photon.superCluster()->etaWidth());
     PhotPhiWidth_->push_back(photon.superCluster()->phiWidth());

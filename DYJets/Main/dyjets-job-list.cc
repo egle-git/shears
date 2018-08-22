16f2f80e5c7950ccd23b68c9e4d577a550f09e21
Louis Moureaux <lmoureau@ulb.ac.be>
2018-08-22 19:54:20 +0200

    Produce commands starting with bin/ in dyjets-job-list

--- DYJets/Main/dyjets-job-list.cc
+++ DYJets/Main/dyjets-job-list.cc
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
             }
 
             for (unsigned job = 0; job < sample.jobs(); ++job) {
-                std::cout << "Main/dyjets-loop -s " << sample.name() << " -o {results}";
+                std::cout << "bin/dyjets-loop -s " << sample.name() << " -o {results}";
                 if (opt.map.count("config") > 0) {
                     std::cout << " -c " << opt.config_file();
                 }

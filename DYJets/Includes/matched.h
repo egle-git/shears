d584cb2fdeea98093b55873d89294170273a5c2d
Louis Moureaux <lmoureau@ulb.ac.be>
2019-01-16 17:49:25 +0100

    Add const versions of matched::apply

--- DYJets/Includes/matched.h
+++ DYJets/Includes/matched.h
@@ -51,6 +51,31 @@ template <class T> class matched
     }
 
     /**
+     * \brief Returns a \c matched object with the result of applying \c f on
+     *        \ref gen and \ref rec.
+     *
+     * The generated function calls are:
+     *
+     *     f(*gen, args...)
+     *     f(*rec, args...)
+     *
+     * If \c gen and/or \c rec is empty, the corresponding value(s) in the
+     * returned will also be empty.
+     */
+    template <class Functor, class... Args>
+    auto apply(Functor &&f, Args &&... args) const
+        -> matched<typename std::decay<decltype(f(*gen, std::forward<Args>(args)...))>::type>
+    {
+        if (gen && rec) {
+            return {f(*gen, std::forward<Args>(args)...), f(*rec, std::forward<Args>(args)...)};
+        } else if (gen) {
+            return {f(*gen, std::forward<Args>(args)...), boost::none};
+        } else {
+            return {boost::none, f(*rec, std::forward<Args>(args)...)};
+        }
+    }
+
+    /**
      * \brief Returns a \c matched object with the result of applying the member
      *        function \c f on \ref gen and \ref rec.
      *
@@ -64,6 +89,31 @@ template <class T> class matched
      */
     template <class MemberFunctor, class... Args>
     auto apply(MemberFunctor &&f, Args &&... args)
+        -> matched<typename std::decay<decltype((*gen.*f)(std::forward<Args>(args)...))>::type>
+    {
+        if (gen && rec) {
+            return {(*gen.*f)(std::forward<Args>(args)...), (*rec.*f)(std::forward<Args>(args)...)};
+        } else if (gen) {
+            return {(*gen.*f)(std::forward<Args>(args)...), boost::none};
+        } else {
+            return {boost::none, (*rec.*f)(std::forward<Args>(args)...)};
+        }
+    }
+
+    /**
+     * \brief Returns a \c matched object with the result of applying the member
+     *        function \c f on \ref gen and \ref rec.
+     *
+     * The generated function calls are:
+     *
+     *     (*gen.*f)(args...)
+     *     (*rec.*f)(args...)
+     *
+     * If \c gen and/or \c rec is empty, the corresponding value(s) in the
+     * returned will also be empty.
+     */
+    template <class MemberFunctor, class... Args>
+    auto apply(MemberFunctor &&f, Args &&... args) const
         -> matched<typename std::decay<decltype((*gen.*f)(std::forward<Args>(args)...))>::type>
     {
         if (gen && rec) {

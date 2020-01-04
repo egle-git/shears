8acb39e726dd163bbb6509e6c85a662ace241548
Louis Moureaux <lmoureau@ulb.ac.be>
2020-01-04 16:16:30 +0100

    Fix segfault in Rochester correction code
    Reported by Andrey Popov:
    
    When parsing the input file, the word read into the buffer 'tag' can be
    4 characters long, which, with the terminating \0, will overflow the
    buffer. Change it to std::string so that the exact length of the word is
    of no concern.
    
    Closes !15.

--- DYJets/Sources/RoccoR.cc
+++ DYJets/Sources/RoccoR.cc
@@ -79,7 +79,7 @@ void RocRes::dumpParams()
 void RocRes::init(std::string filename)
 {
     std::ifstream in(filename.c_str());
-    char tag[4];
+    std::string tag;
     int type, sys, mem, isdt, var, bin;
     std::string s;
     while (std::getline(in, s)) {
@@ -262,7 +262,7 @@ void RocOne::init(std::string filename, int iTYPE, int iSYS, int iMEM)
     RR.init(filename);
 
     std::ifstream in(filename.c_str());
-    char tag[4];
+    std::string tag;
     int type, sys, mem, isdt, var, bin;
 
     bool initialized = false;

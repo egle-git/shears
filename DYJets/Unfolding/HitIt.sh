96043cdfc4cdf20f67235364181485d8c7cfdd59
wtabb <william.robert.tabb@cern.ch>
2022-09-29 19:41:07 +0200

    updated the bash script HitIt.sh in Unfolding

--- DYJets/Unfolding/HitIt.sh
+++ DYJets/Unfolding/HitIt.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# This script gets the histograms which have been combined using combine-ROOT-files.sh
+# This script is designed to do everything all in one step
+# include one argument, the name of the directory containing the results of the analysis
+# It then retrieves unfolding histograms and saves them in a root file
 # Then it runs unfolding on these histograms
 # It then plots the results of unfolding along with other plots
 
 # Setup environment
 source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_100 x86_64-centos7-gcc10-opt
 export LD_PRELOAD=/usr/lib64/libopenblas.so.0
+
+./combine-ROOT-files.sh $1
 
 root -l << EOF
 .L macros/getUnfoldingHistograms.C

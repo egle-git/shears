06d8d340f06fb7777d7077a2051b7d557d1fb1f1
Kyeongpil Lee <kplee@cern.ch>
2023-04-20 17:37:28 +0200

    rewrite to hadd DY mass binned samples and use it for both channels. remove mu channel version

--- DYJets/auto-hadd.sh
+++ DYJets/auto-hadd.sh
@@ -1,19 +1,59 @@
 #! /usr/bin/env bash
 
 echo $1
+if [[ $(grep -L "Done writing output" $1/logs/log-???.txt | wc -l) -ne "0" ]]; then
+    echo "OUTPUT MISSING, look at the following files :"
+    echo "$(grep -L "Done writing output" $1/logs/log-???.txt)"
+    exit 1
+fi
 cd $1/results/ || exit 1
 
-for s in data DYJets; do 
-    echo $s
-    ls dyjets-$s-?.root dyjets-$s-??.root
-    hadd -f dyjets-$s.root dyjets-$s-?.root dyjets-$s-??.root
+# samples processed with multiple jobs: need merging
+sample_multiple=(
+    dyjets-data # both for dyjets-data, dyjets-data-smu (mu channel)
+    dyjets-DYJets_M-10to50
+    dyjets-DYJets_M-50to100
+    dyjets-ST_t-channel_top
+    dyjets-TauTau
+    dyjets-TT
+    dyjets-ZZ
+)
+
+# samples processed with a single job: no merging
+sample_single=(
+    dyjets-DYJets_M-100to200
+    dyjets-DYJets_M-200to400
+    dyjets-DYJets_M-400to500
+    dyjets-DYJets_M-500to700
+    dyjets-DYJets_M-700to800
+    dyjets-DYJets_M-800to1000
+    dyjets-DYJets_M-1000to1500
+    dyjets-DYJets_M-1500to2000
+    dyjets-DYJets_M-2000toInf
+    dyjets-ST_s-channel
+    dyjets-ST_t-channel_antitop
+    dyjets-ST_tW_antitop
+    dyjets-ST_tW_top
+    dyjets-WWTo2L2Nu
+    dyjets-WZ
+    dyjets-GammaGamma
+)
+
+# merging samples in sample_multiple
+for index in ${!sample_multiple[*]}; do
+    echo "Combining ${sample_multiple[$index]}"
+    ls ${sample_multiple[$index]}*.root
+    hadd -f ../${sample_multiple[$index]}.root ${sample_multiple[$index]}*.root
 done
 
-#for s in TT WToLNu; do 
-#    echo $s
-#    hadd -f dyjets-$s.root dyjets-$s-?.root
-#done
+# just copy the samples in sample_single
+for index in ${!sample_single[*]}; do
+    echo "Copying ${sample_single[$index]}"
+    cp -p ${sample_single[$index]}.root ../
+done
 
-#hadd -f tmp.root dyjets-data.root dyjets-data-smu.root; 
-#mv tmp.root dyjets-data.root
+cd ../
+echo "output: "
+ls
+
 

5e47b16ee0c3e9667815f0af5f9f0a42589c8b8f
Kyeongpil Lee <kplee@cern.ch>
2023-04-20 17:39:19 +0200

    apply new cut only when it is reco (should not affect gen)

--- DYJets/Sources/zfinder.cc
+++ DYJets/Sources/zfinder.cc
@@ -141,7 +141,10 @@ bool zfinder::valid(const dilepton &candidate, bool isGEN) const
     else {
       if( candidate.a.v.Pt() < _leadingLepPt ) return false;
     }
-    if(candidate.v.M()/candidate.b.v.Pt() > _newcut ) return false;
+    
+    if( !isGEN) {
+        if(candidate.v.M()/candidate.b.v.Pt() > _newcut ) return false;
+    }
 
     // Check if affected by the 2016 EMTF bug
     //if (std::abs(candidate.a.v.Eta()) > 1.2 && std::abs(candidate.b.v.Eta()) > 1.2

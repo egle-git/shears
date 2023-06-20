94bf6a379e65d79495283d0524c21960e3e52972
wtabb <william.robert.tabb@cern.ch>
2023-06-20 21:35:11 +0200

    updated combine-ROOT-files.sh so that it now checks how many files are present to be copied, then combines them if it's more than 1, then moves them.

--- DYJets/Unfolding/combine-ROOT-files.sh
+++ DYJets/Unfolding/combine-ROOT-files.sh
@@ -9,7 +9,7 @@ saveDirectory=$2
 echo "Combining output histograms from Shears from directory $loadDirectory"
 echo "Saving the combined files to $saveDirectory"
 
-# categori es which have many jobs and need to be combined
+# File stems of all files to be combined and copied
 file_stem=(
     dyjets-data
     dyjets-DYJets_M-10to50
@@ -17,10 +17,7 @@ file_stem=(
     dyjets-TauTau
     dyjets-TT
     dyjets-ZZ
-)
-
-# Categories with single files that only need to be moved
-single_files=(
+    dyjets-ST_t-channel_top
     dyjets-DYJets_M-100to200
     dyjets-DYJets_M-200to400
     dyjets-DYJets_M-400to500
@@ -34,32 +31,52 @@ single_files=(
     dyjets-ST_tW_top
     dyjets-ST_s-channel
     dyjets-ST_t-channel_antitop
-    dyjets-ST_t-channel_top
     dyjets-WWTo2L2Nu
     dyjets-WZ
     dyjets-GammaGamma
 )
+
+# location to save all combined output files
 saveLocation=$saveDirectory
 
+# creates directory for save location if it does not exist
 mkdir -p $saveLocation
 
+# move into save directory
 cd $saveLocation
+# create directory one level up for saving plots if it does not exist
 mkdir -p ../plots
+# remove old root files if they are already present
 rm *.root
 cd -
+# move to location of saved root files from Shears so they can be loope over, combined, and
+# copied
 cd ../$loadDirectory/results
 
 # Loop over each file that needs to be combined and combine with hadd
 for index in ${!file_stem[*]}; do
-    echo "Combining ${file_stem[$index]}"
-    hadd ${file_stem[$index]}.root ${file_stem[$index]}*
-    mv ${file_stem[$index]}.root ../../Unfolding/${saveLocation}/
-done
+    # see how many files with a given stem exist
+    nFiles=$(ls -lh ${file_stem[$index]}* | wc -l)
+    echo "$nFiles ${file_stem[$index]} files in the directory"
 
-# loop over each individual file that nees to be moved and move it
-for index in ${!single_files[*]}; do
-    echo "Moving ${single_files[$index]}"
-    cp ${single_files[$index]}.root ../../Unfolding/${saveLocation}/
+    # if there is one file, it just needs to be copied to the new location
+    if [ $nFiles -eq 1 ]
+    then
+        echo "Copying file ${file_stem[$index]}.root to $saveLocation"
+        cp ${file_stem[$index]}.root ../../Unfolding/${saveLocation}/
+    # if there are multiple files, they need to be combined with hadd
+    # and then copied to the new location
+    elif [ $nFiles -gt 1 ]
+    then
+        echo "Combining files"
+        hadd ${file_stem[$index]}.root ${file_stem[$index]}*
+        echo "Copying file ${file_stem[$index]}.root to $saveLocation"
+        cp ${file_stem[$index]}.root ../../Unfolding/${saveLocation}/
+    # if there are zero files of this type, there is nothing to hadd or copy
+    else 
+        echo "There are no files with the stem ${file_stem[$index]} in the directory $loadDirectory"
+    fi
+    echo " "
 done
 
 cd ../../Unfolding
